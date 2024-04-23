d29cf6707870c21ecc25aa6242cd1f6418463d92
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2024-04-23 11:02:31 +0200

    chapter 13-16

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from './ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Dashbaord',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
